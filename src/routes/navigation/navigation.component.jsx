03a1a6f93a62a97259701e2bab25a3a3cf8e51f5
Adrian <achapa@adrianchapa.dev>
2022-05-13 02:30:22 -0500

    added sign in component and firebase utils/libraries

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+// Navigation is our top level component kinda like our template .. represents thing that should always be present
+// Outlet is what we will swap based on route matching inside / nested route matching
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,77 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+// getDoc and setDoc represent getting the document data and setting the document data
+// doc is what you need to get a document instance
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAFOWL_o13Q9H4_4sS-JQVyL17pYky2iA',
+
+  authDomain: 'crwn-clothing-db-678ff.firebaseapp.com',
+
+  projectId: 'crwn-clothing-db-678ff',
+
+  storageBucket: 'crwn-clothing-db-678ff.appspot.com',
+
+  messagingSenderId: '321352866886',
+
+  appId: '1:321352866886:web:c7c7af63cec14171127e47',
+};
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+// needed to use google authentication
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// create and connect to our database in console
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // takes 3 arguments: a database, collection, identifier
+  // we have user doc reference and from it we got a snapshot which allows
+  //   us to check whether or not there is an instance of it that exists and access the data
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  //   on the object returned from getDoc there is a method called exists
+  //   tells us if the reference and data related to that reference exists
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  //if user data does not exist
+  //create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  // if user data exists
+  return userDocRef;
+
+  // return userDocRef
+};
